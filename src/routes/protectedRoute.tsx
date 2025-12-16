048cd4ce46011dc7cf26f09ec100217067a8edc4
thatipradeepreddy <pradeep.thati@qylis.com>
2025-12-16 10:41:07 +0530

    made chnages

--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -4,5 +4,5 @@ export default function ProtectedRoute() {
 	const raw = localStorage.getItem("userProfile")
 	const isLoggedIn = !!raw
 
-	return isLoggedIn ? <Outlet /> : <Navigate to='/login' replace />
+	return isLoggedIn ? <Outlet /> : <Navigate to='/' replace />
 }
